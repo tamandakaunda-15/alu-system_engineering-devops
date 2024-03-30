69d6baec60f8b62136083677686c9ec84dc8b6d4
tamandakaunda-15 <tamandakaunda900@gmail.com>
2024-03-30 09:30:04 -0700

    fix the 500 error and automate it using puppet

---
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,5 @@
+# Automating the debug using puppet
+exec { 'fix the php extension issue':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
